60d7b837db61dda37296951d5b10c8c39f3f37ce
Henrique Barucco <33501527+HenriqueBarucco@users.noreply.github.com>
2023-04-18 20:52:28 +0000

    fix: PowerBI

--- src/pages/powerbi.js
+++ src/pages/powerbi.js
@@ -1,6 +1,13 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+
+const styles = {
+  iframe: {
+    width: "100vw",
+    height: "100vh",
+    overflow: "hidden",
+    position: "fixed",
+  },
+};
 
 export default function PowerBI() {
   return (
@@ -8,7 +15,13 @@ export default function PowerBI() {
       <Head>
         <title>PowerBI</title>
       </Head>
-        <iframe title="PetShop" src="https://app.powerbi.com/view?r=eyJrIjoiMDk2Zjc3ZDUtZWM3OC00N2M5LTlkY2QtNDQ3YjBmM2IxNGI4IiwidCI6IjNiMThmODUyLTJkZjgtNDE0Ni05ZGU0LWIxYjYyZmRlNDliMyJ9&$toolbarType=0" frameborder="0" allowFullScreen="true"></iframe>
+      <iframe
+        style={styles.iframe}
+        title="PetShop"
+        key="if"
+        src="https://app.powerbi.com/view?r=eyJrIjoiMDk2Zjc3ZDUtZWM3OC00N2M5LTlkY2QtNDQ3YjBmM2IxNGI4IiwidCI6IjNiMThmODUyLTJkZjgtNDE0Ni05ZGU0LWIxYjYyZmRlNDliMyJ9&$toolbarType=0"
+        frameborder="0"
+      ></iframe>
     </div>
   );
 }
